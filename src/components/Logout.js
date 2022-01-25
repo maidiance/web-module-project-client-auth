4d33d2794f8260a08c13a9954a55ddc6c2fd9bb2
Winifred Lu <winnie.lu@comcast.net>
2022-01-25 15:38:50 -0500

    basic logout component complete

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const Logout = () => {
+    const { push } = useHistory();
+
+    useEffect(() => {
+        axiosWithAuth()
+            .post('/logout')
+            .then(resp => {
+                localStorage.removeItem('token');
+                push('/login');
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }, []);
+}
+
+export default Logout;
