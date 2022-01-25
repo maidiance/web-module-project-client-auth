70ef452a378939af6841872efaed2a8cf6c619fe
Winifred Lu <winnie.lu@comcast.net>
2022-01-25 15:34:55 -0500

    basic styling for addFriend in place

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -3,6 +3,46 @@ import styled from 'styled-components';
 
 import axiosWithAuth from './../utils/axiosWithAuth';
 
+const StyledDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 70%;
+    margin: 0 auto;
+    h1 {
+        margin: 1% auto;
+    }
+    form {
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .input-container {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .input-container p {
+        margin-left: 25%;
+        margin-top: 0%;
+        margin-bottom: 0%;
+        width: 100%;
+    }
+    .input-container input {
+        width: 50%;
+        background-color: black;
+        color: white;
+        padding: 2% 0;
+        margin: 1% auto;
+    }
+    button {
+        background-color: black;
+        color: white;
+        padding: 2% 2%;
+        width: 50%;
+        margin: 0 auto;
+    }
+`
+
 class AddFriend extends React.Component {
     state={
         name: '',
@@ -29,24 +69,26 @@ class AddFriend extends React.Component {
 
     render() {
         return(
-            <div>
+            <StyledDiv>
                 <h1>ADD FRIEND</h1>
                 <form onSubmit={this.submit}>
+                    <label className='input-container'><p>FRIEND NAME</p>
                     <input
                         type='text'
                         name='name'
                         value={this.state.name}
                         onChange={this.handleChange}
-                    />
+                    /></label>
+                    <label className='input-container'><p>FRIEND EMAIL</p>
                     <input
                         type='text'
                         name='email'
                         value={this.state.email}
                         onChange={this.handleChange}
-                    />
+                    /></label>
                     <button>SUBMIT</button>
                 </form>
-            </div>
+            </StyledDiv>
         )
     }
 }
